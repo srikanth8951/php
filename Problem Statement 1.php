7e10972c742790ab4bdddb9816aafe4fbd1b51d6
srikanth8951 <42550572+srikanth8951@users.noreply.github.com>
2020-12-03 14:36:14 +0530

    Add files via upload

---
+++ Problem Statement 1.php
@@ -0,0 +1,19 @@
+<?php
+
+$server = readline('Enter number of server');
+$server_load = readline('Enter the server load');
+
+$n = (int)$server;
+$array = array_map('intval', explode(' ', $server));
+
+foreach ($array as $value){
+    if ($value<50) {
+        $n = $n/2;
+    }
+    else {
+        $n = 2*$n+1;
+    }
+    
+}
+echo("After 5 minutes,".$n."server is running");
+?>
