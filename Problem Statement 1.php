7020b5d1c4022435e8f4d3ce1d2db3066b10f632
srikanth8951 <42550572+srikanth8951@users.noreply.github.com>
2020-12-04 14:11:20 +0530

    Add files via upload

--- Problem Statement 1.php
+++ Problem Statement 1.php
@@ -1,57 +1,18 @@
 <?php
 
-$input_1 = readline("Enter the input");//enter number of breads vada and samosa
-$input_2 = readline("Enter the input");// enter price of vada and samosa
+$server = readline('Enter number of server');
+$server_load = readline('Enter the server load');
 
-$arrya_1 = explode(" ",$input_1);
-$arrya_2 = explode(" ",$input_2);
-$bread = (int)$arrya_1[0];
-$vada = (int)$arrya_1[1];
-$samosa = (int)$arrya_1[2];
-
-$Vadapav = (int)$arrya_2[0];
-$Samosapav = (int)$arrya_2[1];
-
-$totalPrice = 0;
-
-if ($Samosapav > $Vadapav) {
-    
-    while($bread>=2){
-        if ($samosa==0 and $vada==0) {
-            break;
-        }
-        if($samosa>0){
-            $totalPrice = $totalPrice + $Samosapav;
-            $samosa = $samosa-1;
-            $bread = $bread-2;
-        }
-        elseif ($vada>0) {
-            $totalPrice = $totalPrice + $Vadapav;
-            $vada = $vada-1;
-            $bread = $bread-2;
-        } 
-        
+$n = (int)$server;
+$array = array_map('intval', explode(' ', $server_load));
+foreach($array as &$value) {
+     if ($value<50) {
+        $n = $n/2;
+    }
+    else {
+         $n = 2*$n+1;
     }
     
 }
-else {
-   while($bread>=2){
-        if ($samosa==0 and $vada==0) {
-            break;
-        }
-        if($vada>0){
-            $totalPrice = $totalPrice + $Vadapav;
-            $vada = $vada-1;
-            $bread = $bread-2;
-        }
-        elseif ($samosa>0) {
-            $totalPrice = $totalPrice + $Samosapav;
-            $samosa = $samosa-1;
-            $bread = $bread-2;
-        } 
-        
-    } 
-}
-echo("Maximum profit possible is Rs.".$totalPrice );
-
+echo("After 5 minutes,".$n."server is running");
 ?>
---
+++ Problem Statement 2.php
@@ -0,0 +1,57 @@
+<?php
+
+$input_1 = readline("Enter the input");//enter number of breads vada and samosa
+$input_2 = readline("Enter the input");// enter price of vada and samosa
+
+$arrya_1 = explode(" ",$input_1);
+$arrya_2 = explode(" ",$input_2);
+$bread = (int)$arrya_1[0];
+$vada = (int)$arrya_1[1];
+$samosa = (int)$arrya_1[2];
+
+$Vadapav = (int)$arrya_2[0];
+$Samosapav = (int)$arrya_2[1];
+
+$totalPrice = 0;
+
+if ($Samosapav > $Vadapav) {
+    
+    while($bread>=2){
+        if ($samosa==0 and $vada==0) {
+            break;
+        }
+        if($samosa>0){
+            $totalPrice = $totalPrice + $Samosapav;
+            $samosa = $samosa-1;
+            $bread = $bread-2;
+        }
+        elseif ($vada>0) {
+            $totalPrice = $totalPrice + $Vadapav;
+            $vada = $vada-1;
+            $bread = $bread-2;
+        } 
+        
+    }
+    
+}
+else {
+   while($bread>=2){
+        if ($samosa==0 and $vada==0) {
+            break;
+        }
+        if($vada>0){
+            $totalPrice = $totalPrice + $Vadapav;
+            $vada = $vada-1;
+            $bread = $bread-2;
+        }
+        elseif ($samosa>0) {
+            $totalPrice = $totalPrice + $Samosapav;
+            $samosa = $samosa-1;
+            $bread = $bread-2;
+        } 
+        
+    } 
+}
+echo("Maximum profit possible is Rs.".$totalPrice );
+
+?>
